f1fa8d6f5c131699e53f32a51efb9820b1d2dbbe
regmarmcem <tknktknktknktknktknk@gmail.com>
2023-02-15 01:27:36 +0900

    [feature] add due to task

--- models/models.go
+++ models/models.go
@@ -6,5 +6,6 @@ type Task struct {
 	ID        int       `json:"task_id"`
 	Title     string    `json:"title"`
 	Detail    string    `json:"detail"`
+	Due       time.Time `json:"due"`
 	CreatedAt time.Time `json:"created_at"`
 }
